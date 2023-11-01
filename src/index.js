9dd54ce8da4507bb15bf46d1b28d1c92c6095591
ytw <a9600125a@gmail.com>
2023-11-01 22:02:00 +0800

    feat: enable css and css modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom/client';
-import { App } from './App.jsx';
+import { render } from 'react-dom';
+import { App } from './App';
+import './index.css';
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = document.getElementById('root');
 
-root.render(<App />);
+render(<App />, root);
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,28 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      // use css modules
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      // use css only
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: [/\.module\.css$/, /node_modules/],
+      },
     ],
   },
   plugins: [
