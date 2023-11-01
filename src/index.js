5bde1f1080f1c2399831380a43647108275777b6
ytw <a9600125a@gmail.com>
2023-11-01 22:07:46 +0800

    fix: use createRoot instead of legacy render api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
-const root = document.getElementById('root');
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-render(<App />, root);
+root.render(<App />);
