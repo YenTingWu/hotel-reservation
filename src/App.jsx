a5ad8476197e527e5fe59964bf42db5d8ac37356
ytw <a9600125a@gmail.com>
2023-11-02 14:01:43 +0800

    feat: implement DemoMonitor

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
 import styles from './App.module.css';
+import { DemoMonitor } from './components/DemoMonitor';
 import { RoomAllocation } from './components/RoomAllocation';
 
 export function App() {
+  const [numberOfPeople, setNumberOfPeople] = useState(10);
+  const [numberOfRoom, setNumberOfRoom] = useState(3);
+  const [result, setResult] = useState(null);
+
   return (
     <div className={styles.app}>
+      <DemoMonitor
+        numberOfPeople={numberOfPeople}
+        numberOfRoom={numberOfRoom}
+        result={result}
+        onNumberOfPeopleChange={(v) => setNumberOfPeople(v)}
+        onNumberOfRoomChange={(v) => setNumberOfRoom(v)}
+      />
       <RoomAllocation
-        guest={9}
-        room={3}
-        onChange={(result) => console.log(result)}
+        guest={numberOfPeople}
+        room={numberOfRoom}
+        onChange={(result) => {
+          console.log(result);
+          setResult(result);
+        }}
       />
     </div>
   );
 }
-
-// 1. CustomInputNumber patterns
---
+++ src/components/DemoMonitor/DemoMonitor.jsx
@@ -0,0 +1,35 @@
+import styles from './DemoMonitor.module.css';
+
+export const DemoMonitor = ({
+  numberOfPeople,
+  numberOfRoom,
+  result,
+  onNumberOfPeopleChange,
+  onNumberOfRoomChange,
+}) => {
+  return (
+    <div className={styles['row-container']}>
+      <div className={styles.row}>
+        <label>Number of People</label>
+        <input
+          value={numberOfPeople}
+          onChange={(e) => {
+            if (isNaN(+e.target.value)) return;
+            onNumberOfPeopleChange(e.target.value);
+          }}
+        />
+      </div>
+      <div className={styles.row}>
+        <label>Number of Room</label>
+        <input
+          value={numberOfRoom}
+          onChange={(e) => {
+            if (isNaN(+e.target.value)) return;
+            onNumberOfRoomChange(e.target.value);
+          }}
+        />
+      </div>
+      <div>{JSON.stringify(result, null, 2)}</div>
+    </div>
+  );
+};
